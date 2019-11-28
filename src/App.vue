c06aadc3ae62ecdf49be70cb93d6f9370c845ec2
Radoslav Tomas <radoslav.tomas@gmail.com>
2019-11-28 14:37:32 +0000

    improved controls

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app" class="font-main p-2">
-    <div class="w-full md:w-1/5 mx-auto mt-10">
+    <div class="w-full md:w-2/5 mx-auto mt-10">
       <div v-if="canvas.length === 0">
         <div class="w-full flex justify-between items-center mb-4">
           <div class="w-full mr-1">
@@ -11,7 +11,9 @@
               v-model="grid.x"
               class="w-full border border-gray-500 rounded px-2 py-3"
             >
-              <option v-for="i in grid.maxX" :key="i" :value="i">{{ i }}</option>
+              <option v-for="i in grid.maxX" :key="i" :value="i">{{
+                i
+              }}</option>
             </select>
           </div>
 
@@ -23,7 +25,9 @@
               v-model="grid.y"
               class="w-full border border-gray-500 rounded px-2 py-3"
             >
-              <option v-for="j in grid.maxX" :key="j" :value="j">{{ j }}</option>
+              <option v-for="j in grid.maxX" :key="j" :value="j">{{
+                j
+              }}</option>
             </select>
           </div>
         </div>
@@ -32,13 +36,15 @@
           <button
             class="w-full border border-gray-500 px-2 py-3 rounded text-white bg-teal-400 hover:bg-teal-500"
             @click="createCanvas"
-          >Create canvas</button>
+          >
+            Create canvas
+          </button>
         </div>
       </div>
 
       <div v-if="canvas.length > 1">
         <div class="mb-4">
-          <label for="mode" class="block">Mode</label>
+          <!-- <label for="mode" class="block">Mode</label>
           <select
             name="mode"
             id="mode"
@@ -49,69 +55,126 @@
             <option value="row">Row</option>
             <option value="column">Column</option>
             <option value="flood">Flood</option>
-          </select>
+          </select> -->
+          <div class="w-full h-12 flex">
+            <div
+              class="w-1/4 h-full border border-gray-500 border-r-0 flex justify-center items-center rounded-l cursor-pointer"
+              :class="{ 'bg-teal-400 text-white': settings.mode === 'single' }"
+              @click="updateMode('single')"
+            >
+              <fa-icon icon="dot-circle"></fa-icon>
+            </div>
+
+            <div
+              class="w-1/4 h-full border border-gray-500 border-r-0 flex justify-center items-center cursor-pointer"
+              :class="{ 'bg-teal-400 text-white': settings.mode === 'row' }"
+              @click="updateMode('row')"
+            >
+              <fa-icon icon="arrows-alt-h"></fa-icon>
+            </div>
+
+            <div
+              class="w-1/4 h-full border border-gray-500 border-r-0 flex justify-center items-center cursor-pointer"
+              :class="{ 'bg-teal-400 text-white': settings.mode === 'column' }"
+              @click="updateMode('column')"
+            >
+              <fa-icon icon="arrows-alt-v"></fa-icon>
+            </div>
+
+            <div
+              class="w-1/4 h-full border border-gray-500 flex justify-center items-center rounded-r cursor-pointer"
+              :class="{ 'bg-teal-400 text-white': settings.mode === 'flood' }"
+              @click="updateMode('flood')"
+            >
+              <fa-icon icon="arrows-alt"></fa-icon>
+            </div>
+          </div>
         </div>
 
         <div class="mb-4">
-          <label for="type" class="block">I want to change</label>
-          <select
-            name="type"
-            id="type"
-            v-model="type"
-            class="w-full border border-gray-500 rounded px-2 py-3"
-          >
-            <option value="shape">Shape</option>
-            <option value="bg">Color</option>
-          </select>
+          <div class="w-full h-12 flex">
+            <div
+              class="w-1/2 h-full border border-gray-500 border-r-0 flex justify-center items-center rounded-l cursor-pointer"
+              :class="{ 'bg-teal-400 text-white': type === 'bg' }"
+              @click="updateType('bg')"
+            >
+              Color
+            </div>
+            <div
+              class="w-1/2 h-full border border-gray-500 flex justify-center items-center rounded-r cursor-pointer"
+              :class="{ 'bg-teal-400 text-white': type === 'shape' }"
+              @click="updateType('shape')"
+            >
+              Shape
+            </div>
+          </div>
         </div>
 
         <div class="mb-4" v-if="type === 'shape'">
-          <label for="shape" class="block">Set shape</label>
-          <select
-            name="shape"
-            id="shape"
-            v-model="settings.shape"
-            class="w-full border border-gray-500 rounded px-2 py-3"
-          >
-            <option value="square">Square</option>
-            <option value="circle">Circle</option>
-          </select>
+          <div class="w-full flex justify-between items-center">
+            <fa-icon
+              v-for="shape in shapes"
+              :key="shape.value"
+              :icon="shape.value"
+              @click="updateShape(shape.value)"
+              class="text-gray-400 text-3xl mr-2 last:mr-0 cursor-pointer"
+              :class="{ 'text-teal-400': settings.shape === shape.value }"
+            ></fa-icon>
+          </div>
         </div>
 
         <div class="mb-4" v-if="type === 'bg'">
-          <label for="color" class="block">Set color</label>
-          <select
-            name="color"
-            id="color"
-            v-model="settings.bg"
-            class="w-full border border-gray-500 rounded px-2 py-3"
-          >
-            <option v-for="color in colors" :key="color.value" :value="color.value">{{ color.name }}</option>
-          </select>
+          <div class="w-full flex justify-between items-center flex-wrap">
+            <div
+              v-for="color in colors"
+              :key="color.value"
+              class="w-1/6 h-10 flex justify-center items-center"
+            >
+              <div
+                @click="updateColor(color.value)"
+                class="rounded-full h-8 w-8 cursor-pointer"
+                :class="[
+                  color.class,
+                  { 'shadow-outline': color.value === settings.bg }
+                ]"
+              ></div>
+            </div>
+          </div>
         </div>
 
         <div class="w-full">
           <button
             class="w-full border border-gray-500 px-2 py-3 rounded text-white bg-teal-400 hover:bg-teal-500"
             @click="resetCanvas"
-          >Reset canvas</button>
+          >
+            Reset canvas
+          </button>
         </div>
       </div>
     </div>
 
-    <div class="mt-10 flex justify-center items-center flex-col" v-if="canvas.length > 1">
+    <div
+      class="mt-10 flex justify-center items-center flex-col"
+      v-if="canvas.length > 1"
+    >
       <div
         class="canvas w-full flex justify-center flex-no-wrap"
         v-for="(row, x) in canvas"
         :key="x"
       >
         <div
-          @click="handleClick(x,y)"
-          class="shape w-4 h-4 sm:w-6 sm:h-6 md:w-8 md:h-8 lg:w-10 lg:h-10 border border-gray-800 m-piccolo cursor-pointer shadow"
+          @click="handleClick(x, y)"
+          class="w-7 h-7 xs:w-8 xs:h-8 md:w-10 md:h-10 m-piccolo cursor-pointer flex justify-center items-center"
           :class="classObject(obj)"
           v-for="(obj, y) in row"
           :key="y"
-        ></div>
+        >
+          <fa-icon
+            class="text-sm sm:text-2xl md:text-3xl"
+            :icon="obj.shape"
+            :class="classObject(obj)"
+          ></fa-icon>
+        </div>
       </div>
     </div>
   </div>
@@ -131,51 +194,97 @@ export default {
       colors: [
         {
           name: "White",
-          value: "white"
+          value: "white",
+          class: "bg-gray-200 border border-gray-400"
         },
         {
           name: "Gray",
-          value: "gray"
+          value: "gray",
+          class: "bg-gray-400"
         },
         {
           name: "Red",
-          value: "red"
+          value: "red",
+          class: "bg-red-400"
         },
         {
           name: "Orange",
-          value: "orange"
+          value: "orange",
+          class: "bg-orange-400"
         },
         {
           name: "Yellow",
-          value: "yellow"
+          value: "yellow",
+          class: "bg-yellow-400"
         },
         {
           name: "Green",
-          value: "green"
+          value: "green",
+          class: "bg-green-400"
         },
         {
           name: "Teal",
-          value: "teal"
+          value: "teal",
+          class: "bg-teal-400"
         },
         {
           name: "Blue",
-          value: "blue"
+          value: "blue",
+          class: "bg-blue-400"
         },
         {
           name: "Indigo",
-          value: "indigo"
+          value: "indigo",
+          class: "bg-indigo-500"
         },
         {
           name: "Purple",
-          value: "purple"
+          value: "purple",
+          class: "bg-purple-400"
         },
         {
           name: "Pink",
-          value: "pink"
+          value: "pink",
+          class: "bg-pink-400"
         },
         {
           name: "Black",
-          value: "black"
+          value: "black",
+          class: "bg-gray-900"
+        }
+      ],
+      shapes: [
+        {
+          name: "Square",
+          value: "square-full"
+        },
+        {
+          name: "Circle",
+          value: "circle"
+        },
+        {
+          name: "Star",
+          value: "star"
+        },
+        {
+          name: "Asterisk",
+          value: "asterisk"
+        },
+        {
+          name: "Snowflake",
+          value: "snowflake"
+        },
+        {
+          name: "Stroopwafel",
+          value: "stroopwafel"
+        },
+        {
+          name: "Dice One",
+          value: "dice-one"
+        },
+        {
+          name: "Dice Five",
+          value: "dice-five"
         }
       ],
       canvas: [],
@@ -195,21 +304,31 @@ export default {
   methods: {
     classObject(obj) {
       return {
-        "bg-gray-100": obj.bg === "white",
-        "bg-gray-900": obj.bg === "black",
-        "bg-gray-400": obj.bg === "gray",
-        "bg-red-400": obj.bg === "red",
-        "bg-orange-400": obj.bg === "orange",
-        "bg-yellow-400": obj.bg === "yellow",
-        "bg-green-400": obj.bg === "green",
-        "bg-teal-400": obj.bg === "teal",
-        "bg-blue-400": obj.bg === "blue",
-        "bg-indigo-400": obj.bg === "indigo",
-        "bg-purple-400": obj.bg === "purple",
-        "bg-pink-400": obj.bg === "pink",
-        "rounded-full": obj.shape === "circle",
-        "rounded-none": obj.shape === "square"
+        "text-gray-200": obj.bg === "white",
+        "text-gray-900": obj.bg === "black",
+        "text-gray-400": obj.bg === "gray",
+        "text-red-400": obj.bg === "red",
+        "text-orange-400": obj.bg === "orange",
+        "text-yellow-400": obj.bg === "yellow",
+        "text-green-400": obj.bg === "green",
+        "text-teal-400": obj.bg === "teal",
+        "text-blue-400": obj.bg === "blue",
+        "text-indigo-500": obj.bg === "indigo",
+        "text-purple-400": obj.bg === "purple",
+        "text-pink-400": obj.bg === "pink"
       };
+    },
+    updateShape(shape) {
+      this.settings.shape = shape;
+    },
+    updateColor(color) {
+      this.settings.bg = color;
+    },
+    updateType(type) {
+      this.type = type;
+    },
+    updateMode(mode) {
+      this.settings.mode = mode;
     },
     handleClick(x, y) {
       this.coords.x = x;
@@ -393,7 +512,7 @@ export default {
           canvas[x][y] = {
             id: this.generateId.next().value,
             coords: [x, y],
-            shape: "square",
+            shape: "square-full",
             bg: "white"
           };
         }
--- src/main.js
+++ src/main.js
@@ -1,5 +1,38 @@
 import Vue from "vue";
 import App from "./App.vue";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import {
+  faSquareFull,
+  faCircle,
+  faAsterisk,
+  faSnowflake,
+  faStar,
+  faStroopwafel,
+  faDiceOne,
+  faDiceFive,
+  faDotCircle,
+  faArrowsAlt,
+  faArrowsAltV,
+  faArrowsAltH
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+
+library.add(
+  faSquareFull,
+  faCircle,
+  faAsterisk,
+  faSnowflake,
+  faStar,
+  faStroopwafel,
+  faDiceOne,
+  faDiceFive,
+  faDotCircle,
+  faArrowsAlt,
+  faArrowsAltV,
+  faArrowsAltH
+);
+
+Vue.component("fa-icon", FontAwesomeIcon);
 
 Vue.config.productionTip = false;
 
